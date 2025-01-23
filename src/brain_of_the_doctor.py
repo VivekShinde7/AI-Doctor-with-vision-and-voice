84a311dcf8ca07770f3bf50409ba73153a3941f0
VivekShinde7 <vivushinde02@gmail.com>
2025-01-23 14:03:07 +0530

    LLM setup done

---
+++ src/brain_of_the_doctor.py
@@ -0,0 +1,40 @@
+import os
+from langchain_groq import ChatGroq
+from groq import Groq
+from dotenv import load_dotenv
+import base64
+
+# Configure the LLM
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
+def analyze_image_with_query(query, model, encoded_image):
+    load_dotenv()
+    groq_api_key = os.getenv("GROQ_API_KEY")
+    client = Groq()
+    
+    messages = [
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": query},
+                {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{encoded_image}"}},
+            ],
+        }
+    ]
+    
+    
+    chat_completion = client.chat.completions.create(
+        messages=messages,
+        model=model
+    )
+
+    return chat_completion.choices[0].message.content
+
+# Example usage
+encoded_image = encode_image(r"F:\AI-Doctor\data\acne.jpg")
+query = "whats wrong with my face"
+model = "llama-3.2-90b-vision-preview"
+response = analyze_image_with_query(query, model, encoded_image)
+print("Response from API:", response)
